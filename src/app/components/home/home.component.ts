e70925028e7e3e72f144eddd5a27f22868ebfc3b
Yomar Miranda <mirandayomar@gmail.com>
2018-08-18 20:20:52 -0500

    Show subreddits int the Home component.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { RedditsService } from '../../services/reddits.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styles: []
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styles: []
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    subreddits: any[] = [];
+    test = '';
 
-  ngOnInit() {
-  }
+    constructor(private _reddits: RedditsService) {
+        this.test = '<h5>Hello World!!</h5>';
+        this._reddits.getSubreddits()
+            .subscribe((data: any) => {
+                this.subreddits = data;
+                console.log('Subreddits >>> ', data);
+            });
+    }
+
+    ngOnInit() {
+    }
 
 }
