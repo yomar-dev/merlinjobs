a48875e055fa4a96c87c96a2b2e61785f265729b
Yomar Miranda <mirandayomar@gmail.com>
2018-08-19 02:01:24 -0500

    New route subreddits.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { SubredditComponent } from './components/subreddit/subreddit.component';
 export const ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'search', component: SearchComponent },
-    { path: 'subreddit', component: SubredditComponent },
+    { path: 'subreddit/:url', component: SubredditComponent },
     { path: '', pathMatch: 'full', redirectTo: 'home' },
     { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
